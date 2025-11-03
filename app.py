d897a275914632e626ee842ef19f63b75c782bbb
Blaine McGraw <bmcgraw@uiowa.edu>
2025-11-03 12:54:22 -0600

    bootstrap update

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # module 10 - Flask Application
 # Blaine McGraw 11/3/2025
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!!"
+    # Instead of returning plain text, render your HTML page
+    return render_template("index.html")
 
 if __name__ == "__main__":
-      app.run(host="0.0.0.0", port=5002, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
